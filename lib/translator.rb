55cada1eeeda1cd2b17a7019c41566e11341012b
Duke Ko <dukhyunko93@gmail.com>
2020-06-15 21:40:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,8 @@ require'pry'
 def load_library(file)
   emoticons = {}
   library = YAML.load_file(file)
-    library.each do |meaning, emoji
+    library.each do |meaning, emoji|
+      
       country = {:enligsh => "", :japanese => ""}
       country.each do |keys, values|
 
