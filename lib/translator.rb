77495aae1a9e8308239c29c19027ed53d94d8845
Duke Ko <dukhyunko93@gmail.com>
2020-06-15 20:42:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,6 +5,7 @@ def load_library(file)
   library = YAML.load_file(file)
     library.each do |meaning, emoji|
       eng,jap = emoji
+      meaning = {:english, :japanese}
 
         if !emoticons[meaning]
           emoticons[meaning] = {}
